16355c035801e368de13fa7a12292c704db4ac21
Qayim <200919@student.upm.edu.my>
2023-03-06 14:15:01 +0800

    update the function to arrow function

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,6 @@
 import './ExpenseDate.css';
 
-function ExpenseDate(props){
+const ExpenseDate = (props) => {
     const expenseDate= props.date;
     const month = expenseDate.toLocaleString('en-US', { month: 'long' });
     const day = expenseDate.toLocaleString('en-US', {day: '2-digit'});
